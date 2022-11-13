6ce1d38fed9f8c44c815edb4c2b5cbe73d83ebd0
George Flinn <flinng@thehutgroup.com>
2022-11-13 20:04:37 +0000

    now fetches total price from backend

--- src/hooks/useCheckout.ts
+++ src/hooks/useCheckout.ts
@@ -1,3 +1,21 @@
-export const useCheckout = () => {
-  
+import axios from "axios";
+import { useMutation } from "@tanstack/react-query";
+import { BasketItem } from "../types/BasketItem";
+
+export const useCheckout = (basket: BasketItem[]) => {
+  const body = basket.map(basketItem => ({
+    productId: basketItem.product.id, 
+    quantity: basketItem.quantity
+  }));
+
+  const evaluateBasketCost = async () => {
+    const response = await axios.post<{totalPrice: string}>('http://localhost:8000/products/price', {
+      basket: body
+    });
+    return response.data;
+  };
+
+  return useMutation({
+    mutationFn: evaluateBasketCost
+  })
 };
--- src/pages/CheckoutPage/CheckoutPage.tsx
+++ src/pages/CheckoutPage/CheckoutPage.tsx
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import Navbar from "../../components/Navbar/Navbar";
 import { RootState } from "../../store/store";
 import BasketItemCard from "./components/BasketItemCard/BasketItemCard";
+import { useCheckout } from "../../hooks/useCheckout";
 import "./CheckoutPage.scss";
 
 const CheckoutPage = () => {
   const basket = useSelector((state: RootState) => state.basketReducer);
+  const {data, isLoading, isError, mutate} = useCheckout(basket);
+
+  useEffect(() => {
+    mutate();
+  }, [basket]);
 
   return (
     <div className="CheckoutPage">
       <Navbar />
       <div className="CheckoutPage__basket">
+        <h2>Total Price: £{data?.totalPrice}</h2>
         {basket.map((basketItem) => (
           <BasketItemCard basketItem={basketItem} />
         ))}
