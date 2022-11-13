fe48697531bf602e72ac6556c8803d081abd8653
George Flinn <flinng@thehutgroup.com>
2022-11-13 15:17:44 +0000

    card content improved

--- src/pages/StorePage/components/ProductCard/ProductCard.tsx
+++ src/pages/StorePage/components/ProductCard/ProductCard.tsx
@@ -20,9 +20,15 @@ const ProductCard = ({
       <div className="product-card__description">
         <p>{description}</p>
       </div>
+      <div className="product-card__stock">
+        {unitsInStock > 0 ? <span>{unitsInStock} remaining!</span> : <span>Sold out!</span>}
+      </div>
       <div className="product-card__price">
         <span className={hasDiscount ? "product-card__price__crossed-out" : ""}>£{basePrice.toFixed(2)}</span>
         {hasDiscount && <span className="product-card__price__discounted">Now £{salePrice.toFixed(2)}</span>}
+      </div>
+      <div className="product-card__actions">
+        <button className="product-card__actions__add-to-cart">Add to cart</button>
       </div>
     </div>
   );
